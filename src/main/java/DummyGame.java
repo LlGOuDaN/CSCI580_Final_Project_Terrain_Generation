392b2698030dcef6499f7cc8256c25b33438b6b2
Yifei Li <liy12@rose-hulman.edu>
2021-11-26 01:33:23 -0800

    prgress

---
+++ src/main/java/DummyGame.java
@@ -0,0 +1,113 @@
+import engine.*;
+import engine.graph.Camera;
+import engine.graph.Renderer;
+import engine.graph.lights.DirectionalLight;
+import engine.items.Terrain;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public class DummyGame implements IGameLogic {
+
+    private static final float MOUSE_SENSITIVITY = 0.2f;
+
+    private final Vector3f cameraInc;
+
+    private final Renderer renderer;
+
+    private final Camera camera;
+
+    private Scene scene;
+
+    private float lightAngle;
+
+    private static final float CAMERA_POS_STEP = 0.05f;
+
+    public DummyGame() {
+        renderer = new Renderer();
+        camera = new Camera();
+        cameraInc = new Vector3f(0.0f, 0.0f, 0.0f);
+        lightAngle = -90;
+    }
+
+    @Override
+    public void init(Window window) throws Exception {
+        renderer.init(window);
+
+        scene = new Scene();
+
+        float terrainScale = 10;
+        int terrainSize = 3;
+        float minY = -0.1f;
+        float maxY = 0.1f;
+        int textInc = 40;
+        Terrain terrain = new Terrain(terrainSize, terrainScale, minY, maxY, "./src/textures/heightmap.png", "./src/textures/terrain.png", textInc);
+        scene.setGameItems(terrain.getGameItems());
+
+        // Setup Lights
+        setupLights();
+
+        camera.getPosition().x = 0.0f;
+        camera.getPosition().z = 0.0f;
+        camera.getPosition().y = -0.2f;
+        camera.getRotation().x = 10.f;
+    }
+
+    private void setupLights() {
+        SceneLight sceneLight = new SceneLight();
+        scene.setSceneLight(sceneLight);
+
+        // Ambient Light
+        sceneLight.setAmbientLight(new Vector3f(0.3f, 0.3f, 0.3f));
+
+        // Directional Light
+        float lightIntensity = 1.0f;
+        Vector3f lightPosition = new Vector3f(1, 1, 0);
+        sceneLight.setDirectionalLight(new DirectionalLight(new Vector3f(1, 1, 1), lightPosition, lightIntensity));
+    }
+
+    @Override
+    public void input(Window window, MouseInput mouseInput) {
+        cameraInc.set(0, 0, 0);
+        if (window.isKeyPressed(GLFW_KEY_W)) {
+            cameraInc.z = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_S)) {
+            cameraInc.z = 1;
+        }
+        if (window.isKeyPressed(GLFW_KEY_A)) {
+            cameraInc.x = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_D)) {
+            cameraInc.x = 1;
+        }
+        if (window.isKeyPressed(GLFW_KEY_Z)) {
+            cameraInc.y = -1;
+        } else if (window.isKeyPressed(GLFW_KEY_X)) {
+            cameraInc.y = 1;
+        }
+    }
+
+    @Override
+    public void update(float interval, MouseInput mouseInput) {
+        // Update camera based on mouse
+        if (mouseInput.isRightButtonPressed()) {
+            Vector2f rotVec = mouseInput.getDisplVec();
+            camera.moveRotation(rotVec.x * MOUSE_SENSITIVITY, rotVec.y * MOUSE_SENSITIVITY, 0);
+        }
+
+        // Update camera position
+        camera.movePosition(cameraInc.x * CAMERA_POS_STEP, cameraInc.y * CAMERA_POS_STEP, cameraInc.z * CAMERA_POS_STEP);
+    }
+
+    @Override
+    public void render(Window window) {
+        renderer.render(window, camera, scene);
+    }
+
+    @Override
+    public void cleanup() {
+        renderer.cleanup();
+        scene.cleanup();
+    }
+
+}
